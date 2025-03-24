19d630c2db385a65064586527ced31485a32f948
taronja4 <153595761+taronja4@users.noreply.github.com>
2025-03-24 08:21:49 +0100

    Update FormaTest.java

--- FormaTest.java
+++ FormaTest.java
@@ -18,19 +18,8 @@ class FormaTest {
         Cilindro ci = new Cilindro(5, 6);
         Rectangulo re = new Rectangulo(3, 4);
         Esfera es = new Esfera(6);
-        assertEquals(471.23889803846896,ci.area());
-        assertEquals(12.0,re.area());
-        assertEquals(452.3893421169302,es.area());
-    }
-
-    @Test
-    void testPintura(){
-        Pintura pi=new Pintura(250);
-        Cilindro ci = new Cilindro(5, 6);
-        Rectangulo re = new Rectangulo(3, 4);
-        Esfera es = new Esfera(6);
-        assertEquals(1.8849555921538759,pi.CantidadAPintar(ci));
-        assertEquals(0.048,pi.CantidadAPintar(re));
-        assertEquals(1.8095573684677209,pi.CantidadAPintar(es));
+        assertEquals(471.23,ci.area(),0.01);
+        assertEquals(12.0,re.area(),0.01);
+        assertEquals(452.38,es.area(),0.01);
     }
 }
